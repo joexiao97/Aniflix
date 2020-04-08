7fd919c137e715ff3954059810acdf7795817a92
Shaohua Xiao <joexiao97@gmail.com>
2020-04-08 18:29:23 -0400

    homepage update

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -5,6 +5,9 @@ class Homepage extends React.Component{
 
     constructor(props) {
         super(props);
+        this.state = {
+            selected: false,
+        }
         this.handleLogout = this.handleLogout.bind(this);
     }
 
@@ -13,19 +16,41 @@ class Homepage extends React.Component{
         this.props.logout();
     }
 
+    handleSelected(){
+        e => e.target.classList.add(" selected-nav")
+    }
+
     render(){
         return (
             <>
-            <header className="homepage">
+            {/* <header className="homepage">
                 <div>
                     <h1>Testing Homepage</h1>
                 </div>
-            </header>
-                <h1>HOMEPAGE, WELCOME {this.props.username}</h1>
-                <Link className="logout-btn hover-box" onClick={this.handleLogout} to="/">Log Out</Link>
-            <div>
-                
+            </header> */}
+            <div className="nav-bar">
+                <div className="left-bar">
+                    <div className="nav-sub-btns">
+                            <Link className="browse-logo hover-box nav-sub-logo" onCLick={this.handleSelected()} to="/browse">ANIFLIX</Link>
+                            <Link className="nav-home hover-box nav-sub-c" onCLick={this.handleSelected()} to="/browse" >Home</Link>
+                            <Link className="nav-tv-shows hover-box nav-sub-c" onCLick={this.handleSelected()} to="/browse" >TV Shows</Link>
+                            <Link className="nav-movies hover-box nav-sub-c" onCLick={this.handleSelected()} to="/browse" >Movies</Link>
+                            <Link className="nav-mylist hover-box nav-sub-c" onCLick={this.handleSelected()} to="/my-list" >My List</Link>
+                    </div>
+                </div>
+
+                <div className="right-bar">
+                    <Link className="logout-btn hover-box" onClick={this.handleLogout} to="/">Log Out</Link>
+
+                </div>
             </div>
+
+            <div className="homepage-browse">
+
+
+
+            </div>
+
             </>
         );
     }
