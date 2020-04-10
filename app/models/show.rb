037a004200ac85277838c0fc1732f75686ad06d2
Shaohua Xiao <joexiao97@gmail.com>
2020-04-10 13:59:33 -0400

    updated

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,6 @@
 #  id           :bigint           not null, primary key
 #  title        :string           not null
 #  description  :string
-#  genres       :string           not null
 #  show_type    :string           not null
 #  runtime      :integer          not null
 #  release_date :date             not null
@@ -15,7 +14,7 @@
 #  updated_at   :datetime         not null
 #
 class Show < ApplicationRecord
-    validates :title, :genres, :show_type, :runtime, :cast, presence: true
+    validates :title, :show_type, :runtime, :cast, presence: true
 
     has_one_attached :picture
     has_one_attached :video
